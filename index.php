a6eec1366fa6ca516b40e68007207a73e9d85ef8
ioxusEosa <arshayumann150@gmail.com>
2026-04-26 14:06:34 +0500

    blehh

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100;
+            margin: 0;
+            font-family: 'Lucida Sans';
+        }
+    </style>
+</head>
+<body>
+    
+    <?php 
+    
+    $instruments = [
+        "Cello",
+        "Violin",
+        "Viola"
+    ];
+
+    $goated = "true";
+    $message = "";
+
+    if ($goated){
+        $message = "im losing it";
+    }
+
+    ?>
+
+    <h1>
+    
+        <?=
+        $message . " ". $instruments[0]; 
+        ?>    
+        
+    </h1>
+  
+    <a href="https://i2.wp.com/images.genshin-builds.com/genshin/characters/nefer/image.png?strip=all&quality=100&w=160">aaaaaaaaaaaaa</a>
+  
+    <img src="nefer2.gif" alt="lonely lonely i guess im lonely" width="800" height="400">
+    
+    <p title="AAAAAAAAAAAAAAAAA">
+        hunger
+    </p>
+
+    <ul>
+        <?php foreach ($instruments as $instrument) : ?>
+            <li><?= $instrument ?></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
