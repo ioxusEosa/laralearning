fd62d94f396ad54bfd0bea6125cabbfdaae8096e
ioxusEosa <arshayumann150@gmail.com>
2026-04-28 13:51:29 +0500

    moved HTML elements from index.php to index.view.php

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Demo</title>
-    <style>
-        body {
-            display: grid;
-            place-items: center;
-            height: 100;
-            margin: 0;
-            font-family: 'Lucida Sans';
-        }
-    </style>
-</head>
-<body>
-    
-    <?php 
+<?php 
     
     $characters = [
         [
@@ -69,33 +52,18 @@
         return $filteredItems;
     }
 
-    $filteredCharacters = filter($characters, 'region', 'Nod Krai');
-
-    ?>
-
-    <h1>
-    
-     
-        
-    </h1>
-  
-    <a href="https://i2.wp.com/images.genshin-builds.com/genshin/characters/nefer/image.png?strip=all&quality=100&w=160">aaaaaaaaaaaaa</a>
-  
-    <img src="nefer2.gif" alt="lonely lonely i guess im lonely" width="800" height="400">
-    
-    <p title="AAAAAAAAAAAAAAAAA">
-        hunger
-    </p>
+    $filteredCharacters = filter($characters, 'region', 'Nod Krai'); 
 
 
-    <ul>
-        <?php foreach ($filteredCharacters as $character) : ?>
-            <li>
-                <a href="<?= $character['link']?>">
-                    <?= $character['name'] ?>
-                </a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</body>
-</html>
+    require "index.view.php";
+
+
+
+
+
+
+
+
+
+
+?>
