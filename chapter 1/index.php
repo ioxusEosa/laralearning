8001009926397e0df3a4c9a6fd43fa9109dd8949
ioxusEosa <arshayumann150@gmail.com>
2026-04-28 23:23:08 +0500

    moved stuff into chapter 1 folder or smth

--- chapter 1/index.php
+++ chapter 1/index.php
@@ -52,27 +52,7 @@
         return $filteredItems;
     }
 
-<<<<<<< HEAD
-    $filteredCharacters = filter($characters, 'element', 'Hydro');
-
-    ?>
-
-    <h1>
-    
-     
-        
-    </h1>
-  
-    <a href="https://i2.wp.com/images.genshin-builds.com/genshin/characters/nefer/image.png?strip=all&quality=100&w=160">aaaaaaaaaaaaa</a>
-  
-    <img src="nefer2.gif" alt="lonely lonely i guess im lonely" width="800" height="400">
-    
-    <p title="AAAAAAAAAAAAAAAAA">
-        hunger
-    </p>
-=======
     $filteredCharacters = filter($characters, 'region', 'Nod Krai'); 
->>>>>>> fd62d94f396ad54bfd0bea6125cabbfdaae8096e
 
 
     require "index.view.php";
